4c98bc25e96e2e7eb3a9c52097958bce236e5704
Ralul_888 <74320658+Ralul@users.noreply.github.com>
2026-02-26 15:27:55 +0100

    Initial Commit

---
+++ IP-Midi-Setzer/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using IP_Midi_Setzer;
+
+Console.WriteLine("Hello, World!");
+
+using var sender = new Sender();
+
+while (true)
+{
+    // Send a Note On (Channel 1, Middle C, Velocity 100)
+    sender.SendNoteOn(0, 60, 100);
+
+// Send a Note Off
+    sender.SendNoteOff(0, 60);
+
+// Send Control Change (e.g., Volume on Channel 1)
+    sender.SendControlChange(0, 7, 100);
+
+// Send Program Change
+    sender.SendProgramChange(0, 0);
+
+// Send Pitch Bend (center = 0)
+    sender.SendPitchBend(0, 0);
+
+// Send raw MIDI bytes directly
+    sender.Send([0x90, 0x3C, 0x7F]);
+    
+    Task.Delay(1000).Wait();
+}
