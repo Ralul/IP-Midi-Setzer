b3f66dad1673cea97c2a25c6d161d61070ab3927
emilp <emilp@example.com>
2026-04-12 15:36:36 +0200

    Adds Pin Readout

--- Pi-Stops-Input/Program.cs
+++ Pi-Stops-Input/Program.cs
@@ -1,33 +1,54 @@
 ﻿using System.Device.Gpio;
+using Core;
+using DotNetEnv;
+
+namespace Pi_Stops_Input;
 
 class Program
 {
     
     static void Main()
     {
-        const int s1Pin = 17; // Select/Address pin
-        const int s2Pin = 18; // Select/Address pin
-        const int s3Pin = 27; // Select/Address pin
-        const int s4Pin = 22; // Select/Address pin
+        Env.Load();
+        using var sender = new Sender(interfaceName: Environment.GetEnvironmentVariable("NETWORK_INTERFACENAME"));
+        
+        const int s0Pin = 17; // Select/Address pin
+        const int s1Pin = 18; // Select/Address pin
+        const int s2Pin = 27; // Select/Address pin
+        const int s3Pin = 22; // Select/Address pin
         const int eoPin = 23; // Enable for all switches ON/OFF
         const int sigPin = 24; // Common input or output.
         
         using GpioController controller = new GpioController();
+        controller.OpenPin(s0Pin, PinMode.Output);
         controller.OpenPin(s1Pin, PinMode.Output);
         controller.OpenPin(s2Pin, PinMode.Output);
         controller.OpenPin(s3Pin, PinMode.Output);
-        controller.OpenPin(s4Pin, PinMode.Output);
         controller.OpenPin(eoPin, PinMode.Output);
         controller.OpenPin(sigPin, PinMode.Input);
         
         Console.WriteLine("Pi-Stops-Input. Press Ctrl+C to exit.");
         while (true)
         {
-            
-            // controller.Write(ledPin, PinValue.High);
-            // Thread.Sleep(1000);
-            // controller.Write(ledPin, PinValue.Low);
-            // Thread.Sleep(1000);
+            for (int i = 0; i < 16; i++)
+            {
+                var isS0PinOn = (i & 1) != 0;
+                var isS1PinOn = (i & 2) != 0;
+                var isS2PinOn = (i & 4) != 0;
+                var isS3PinOn = (i & 8) != 0;
+
+                controller.Write(s0Pin, isS0PinOn ? PinValue.High : PinValue.Low);
+                controller.Write(s1Pin, isS1PinOn ? PinValue.High : PinValue.Low);
+                controller.Write(s2Pin, isS2PinOn ? PinValue.High : PinValue.Low);
+                controller.Write(s3Pin, isS3PinOn ? PinValue.High : PinValue.Low);
+
+                var isToggled = controller.Read(sigPin);
+
+                if (isToggled == PinValue.High)
+                {
+                    Console.WriteLine($"{i}: {isToggled}");
+                }
+            }
         }
     }
 }
