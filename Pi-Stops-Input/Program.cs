ffbf3d15b4b49f8f97f367ed733a394fbefa467c
Ralul_888 <74320658+Ralul@users.noreply.github.com>
2026-05-09 13:22:00 +0200

    fix

--- Pi-Stops-Input/Program.cs
+++ Pi-Stops-Input/Program.cs
@@ -44,8 +44,8 @@ class Program
                 {
                     Console.WriteLine($"Stop {i} toggled -> {(currentState ? "ON" : "OFF")}");
 
-                    int baseNote = i * 2 + 24;
-                    int note = currentState ? baseNote : baseNote + 1;
+                    int baseNote = i * 2 + 25;
+                    int note = currentState ? baseNote : baseNote - 1;
 
                     sender.SendNoteOn(SequencerDefinition.CHANEL_STOPS_1_126, note);
                     Task.Delay(100).Wait();
