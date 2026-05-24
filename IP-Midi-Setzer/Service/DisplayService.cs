4e2e4c7517b582eb8eea21009269245684628280
Ralul_888 <74320658+Ralul@users.noreply.github.com>
2026-05-24 19:59:00 +0200

    test

--- IP-Midi-Setzer/Service/DisplayService.cs
+++ IP-Midi-Setzer/Service/DisplayService.cs
@@ -3,44 +3,50 @@ using Iot.Device.CharacterLcd;
 
 namespace IP_Midi_Setzer.Service;
 
-public class DisplayService
+public class DisplayService : IDisposable
 {
+    private readonly GpioController _controller;
     private readonly Lcd2004 _lcd;
-    
+
     public DisplayService()
     {
-        // const int rs = 5;
-        // const int rw = 6;
-        // const int en = 13;
-        //
-        // int[] dataPins = { 19, 26, 21, 20, 16, 12, 1, 7 };
-        //
-        // const int backlight = -1;
-        //
-        // using var controller = new GpioController();
-        //
-        // _lcd = new Lcd2004(
-        //     registerSelectPin: rs,
-        //     enablePin: en,
-        //     dataPins: dataPins,
-        //     backlightPin: backlight,
-        //     backlightBrightness: 1.0f,
-        //     readWritePin: rw,
-        //     controller: controller
-        // );
-        //
-        // _lcd.Clear();
-        //
-        // _lcd.DisplayOn = true;
-        // _lcd.UnderlineCursorVisible = false;
-        // _lcd.BlinkingCursorVisible = false;
-        //
-        // _lcd.SetCursorPosition(0, 0);
+        const int rs = 5;
+        const int rw = 6;
+        const int en = 13;
+
+        int[] dataPins = { 19, 26, 21, 20, 16, 12, 1, 7 };
+
+        const int backlight = -1;
+
+        _controller = new GpioController();
+
+        _lcd = new Lcd2004(
+            registerSelectPin: rs,
+            enablePin: en,
+            dataPins: dataPins,
+            backlightPin: backlight,
+            backlightBrightness: 1.0f,
+            readWritePin: rw,
+            controller: _controller
+        );
+
+        _lcd.Clear();
+        _lcd.DisplayOn = true;
+        _lcd.UnderlineCursorVisible = false;
+        _lcd.BlinkingCursorVisible = false;
+        _lcd.SetCursorPosition(0, 0);
     }
 
     public void ShowNumber(int number)
     {
-        // _lcd.Clear();
-        // _lcd.Write(number.ToString());
+        _lcd.Clear();
+        _lcd.Write(number.ToString());
+    }
+
+    public void Dispose()
+    {
+        _lcd.Dispose();
+        _controller.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
